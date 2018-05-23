57a0a70141a924204dbaa6730764aa412fa93901
Thiago de Freitas Figueiredo <thiagofigueiredo777@gmail.com>
2018-05-23 15:21:31 -0300

    Update ParserT.hs

--- src/Data/HelloWorld/Data/ParserT.hs
+++ src/Data/HelloWorld/Data/ParserT.hs
@@ -7,6 +7,6 @@ import Data.HelloWorld.Data.Types
 
 import Data.SPL
 
--- ParserT -> this would go on Parser/Transformation.hs
+
 hwParser :: Parsec String () (Transformation HwAsset)
 hwParser = string "setMessage(\"" >> many1 letter >>= \s -> string "\")" >> return (setMessage s)
