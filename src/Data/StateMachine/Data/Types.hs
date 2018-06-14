c88b7d9c2eb5d258d0d5b5182c41c5f84119465d
thi4go <thiagofigueiredo777@gmail.com>
2018-06-14 15:29:46 -0300

    simulating a vendingmachine as a statemachine asset; testing is being done in Types definition

--- src/Data/StateMachine/Data/Types.hs
+++ src/Data/StateMachine/Data/Types.hs
@@ -1,32 +1,52 @@
 module Data.StateMachine.Data.Types where
 
-import Data.SPL
+-- import Data.SPL
 
-data State = State String
+data State =
+    InitialState String
+  | FinalState String
+  | State String
+  deriving (Show)
 
-type Transition = State -> State
+type Transition = (State, State)
 
-data StateMachine = 
+data StateMachine =
   StateMachine {
-    initialState :: State ,
 
     states :: [State],
 
     transitions :: [Transition]
-
   }
+  deriving (Show)
+
+-- Type checks
+-- - A state in a transition MUST exist in states
+-- - Exists InitialState and FinalState
+
+vendingMachine :: StateMachine
+vendingMachine =
+  StateMachine
+    [InitialState "start", State "insertMoney", State "countMoney", State "buyProduct", State "moneyError", FinalState "deliverProduct"]
+    [(InitialState "start", State "insertMoney"),
+     (State "insertMoney", State "countMoney"),
+     (State "countMoney", State "buyProduct"),
+     (State "buyProduct", State "moneyError"),
+     (State "moneyError", State "insertMoney"),
+     (State "buyProduct", FinalState "deliverProduct")
+    ]
+
 
 
 -- Transformations
-
-setInitialState :: State -> Transformation StateMachine
-setInitialState s _ (Product p) = 
-  Product $ p { initialState = s }
-
-setTransitionsList :: [Transition] -> Transformation StateMachine
-setTransitionsList ts _ (Product p) =
-  Product $ p { transitions = ts }
-
-setStates :: [State] -> Transformation StateMachine
-setStates ss _ (Product p) =
-  Product $ p { states = ss }
+--
+-- setInitialState :: State -> Transformation StateMachine
+-- setInitialState s _ (Product p) =
+--   Product $ p { initialState = s }
+--
+-- setTransitionsList :: [Transition] -> Transformation StateMachine
+-- setTransitionsList ts _ (Product p) =
+--   Product $ p { transitions = ts }
+--
+-- setStates :: [State] -> Transformation StateMachine
+-- setStates ss _ (Product p) =
+--   Product $ p { states = ss }
